a51ce6f968094d51bfea79c4acf46d149d60366c
Joris Hartog <jorishartog@hotmail.com>
2023-12-11 10:06:29 +0100

    2023: refactor day eleven!

--- 2023/day_11/solution/src/main.rs
+++ 2023/day_11/solution/src/main.rs
@@ -8,11 +8,8 @@ struct Galaxy {
 
 #[derive(Debug, Default)]
 struct Universe {
-    expansion: u64,
     galaxies: Vec<Galaxy>,
     location_map: Vec<Vec<(usize, usize)>>,
-    width: usize,
-    height: usize,
 }
 
 impl Universe {
@@ -34,46 +31,48 @@ impl Universe {
 
         let height = spots.len();
         let width = spots[0].len();
-        let galaxies = spots.into_iter().flatten().flatten().collect();
+        let galaxies: Vec<Galaxy> = spots.into_iter().flatten().flatten().collect();
+        let location_map = Self::calculate_location_map(&galaxies, width, height, expansion);
 
-        let mut universe = Self {
-            expansion,
+        Self {
             galaxies,
-            width,
-            height,
-            ..Default::default()
-        };
-        universe.calculate_location_map();
-        universe
+            location_map,
+        }
     }
 
-    fn calculate_location_map(&mut self) {
+    fn calculate_location_map(
+        galaxies: &[Galaxy],
+        width: usize,
+        height: usize,
+        expansion: u64,
+    ) -> Vec<Vec<(usize, usize)>> {
         // Calculate location map to account for expanding of the universe
-        self.location_map = vec![];
+        let mut location_map = vec![];
         let mut non_empty_rows: HashSet<usize> = HashSet::new();
         let mut non_empty_cols: HashSet<usize> = HashSet::new();
 
-        for galaxy in self.galaxies.iter() {
+        for galaxy in galaxies.iter() {
             non_empty_rows.insert(galaxy.location.0);
             non_empty_cols.insert(galaxy.location.1);
         }
 
         let mut dx = 0;
-        for x in 0..self.width {
+        for x in 0..width {
             let mut row = vec![];
             if !non_empty_rows.contains(&x) {
-                dx += self.expansion as usize;
+                dx += expansion as usize;
             } else {
                 let mut dy = 0;
-                for y in 0..self.height {
+                for y in 0..height {
                     if !non_empty_cols.contains(&y) {
-                        dy += self.expansion as usize;
+                        dy += expansion as usize;
                     }
                     row.push(((x + dx), (y + dy)));
                 }
             }
-            self.location_map.push(row);
+            location_map.push(row);
         }
+        location_map
     }
 
     fn galaxy_pairs(&self) -> Vec<(&Galaxy, &Galaxy)> {
@@ -84,25 +83,20 @@ impl Universe {
             .collect()
     }
 
+    fn get_expanded_location(&self, pos: (usize, usize)) -> (usize, usize) {
+        self.location_map[pos.0][pos.1]
+    }
+
     fn distance(&self, a: &Galaxy, b: &Galaxy) -> u64 {
-        let position_a = self.location_map[a.location.0][a.location.1];
-        let position_b = self.location_map[b.location.0][b.location.1];
+        let position_a = self.get_expanded_location(a.location);
+        let position_b = self.get_expanded_location(b.location);
         ((position_a.0 as i64 - position_b.0 as i64).abs()
             + (position_a.1 as i64 - position_b.1 as i64).abs()) as u64
     }
 }
 
-fn solve_problem_1(input: String) -> u64 {
-    let universe = Universe::new(input, 1);
-    universe
-        .galaxy_pairs()
-        .iter()
-        .map(|(a, b)| universe.distance(a, b))
-        .sum()
-}
-
-fn solve_problem_2(input: String) -> u64 {
-    let universe = Universe::new(input, 1_000_000 - 1);
+fn solve_problem(input: String, expansion: u64) -> u64 {
+    let universe = Universe::new(input, expansion);
     universe
         .galaxy_pairs()
         .iter()
@@ -113,10 +107,10 @@ fn solve_problem_2(input: String) -> u64 {
 fn main() {
     let content = fs::read_to_string("../input").expect("Should have been able to read the file");
 
-    let solution = solve_problem_1(content.clone());
+    let solution = solve_problem(content.clone(), 1);
     println!("Solution part 1: {}", solution);
 
-    let solution = solve_problem_2(content);
+    let solution = solve_problem(content, 1_000_000 - 1);
     println!("Solution part 2: {}", solution);
 }
 
@@ -128,7 +122,7 @@ mod tests {
     fn problem_1_solved() {
         let content = fs::read_to_string("../input-example-1")
             .expect("Should have been able to read the file");
-        assert_eq!(solve_problem_1(content), 374);
+        assert_eq!(solve_problem(content, 1), 374);
     }
 
     #[test]
@@ -153,12 +147,6 @@ mod tests {
     fn problem_2_solved() {
         let content = fs::read_to_string("../input-example-2")
             .expect("Should have been able to read the file");
-        let universe = Universe::new(content, 100 - 1);
-        let distance_sum: u64 = universe
-            .galaxy_pairs()
-            .iter()
-            .map(|(a, b)| universe.distance(a, b))
-            .sum();
-        assert_eq!(distance_sum, 8410);
+        assert_eq!(solve_problem(content, 100 - 1), 8410);
     }
 }
